f19532f90723f018ab09ed99e9fec3976ec76db5
peop1e <chr24@mails.tsinghua.edu.cn>
2025-02-23 16:35:42 +0800

    Convert tab to four spaces in PvP

--- pvp/check/manager.cpp
+++ pvp/check/manager.cpp
@@ -6,32 +6,32 @@
 using namespace std;
 
 int provide_random_number() {
-  // Chosen by fair dice roll.
-  return 4;
+    // Chosen by fair dice roll.
+    return 4;
 }
 
 int main(int argc, char **argv) {
-	signal(SIGPIPE, SIG_IGN);
+    signal(SIGPIPE, SIG_IGN);
 
-	FILE *fin, *fout, *fifo1_in, *fifo1_out, *fifo2_in, *fifo2_out;
+    FILE *fin, *fout, *fifo1_in, *fifo1_out, *fifo2_in, *fifo2_out;
 
-	fin = fopen("input.txt", "r");
-	fout = fopen("output.txt", "w");
-	fifo1_in = fopen(argv[2], "w");
-	fifo1_out = fopen(argv[1], "r");
-	fifo2_in = fopen(argv[4], "w");
-	fifo2_out = fopen(argv[3], "r");
+    fin = fopen("input.txt", "r");
+    fout = fopen("output.txt", "w");
+    fifo1_in = fopen(argv[2], "w");
+    fifo1_out = fopen(argv[1], "r");
+    fifo2_in = fopen(argv[4], "w");
+    fifo2_out = fopen(argv[3], "r");
 
-	int a, b, res;
+    int a, b, res;
     int c = provide_random_number();
 
-	fscanf(fin, "%d %d", &a, &b);
-	fprintf(fifo1_in, "%d %d\n", a, b);
-	fflush(fifo1_in);
-	fprintf(fifo2_in, "%d %d\n", a, b);
-	fflush(fifo2_in);
+    fscanf(fin, "%d %d", &a, &b);
+    fprintf(fifo1_in, "%d %d\n", a, b);
+    fflush(fifo1_in);
+    fprintf(fifo2_in, "%d %d\n", a, b);
+    fflush(fifo2_in);
 
-	double sc1, sc2;
+    double sc1, sc2;
 
     while (true) {
       fscanf(fifo1_out, "%d", &res);
@@ -44,28 +44,28 @@ int main(int argc, char **argv) {
         break;
       }
     }
-	sc1 = (res == a + b + c) ? 1.0 : 0.0;
+    sc1 = (res == a + b + c) ? 1.0 : 0.0;
 
-	while (true) {
-		fscanf(fifo2_out, "%d", &res);
-		if (res < 0) {
-		  fprintf(fifo2_in, "%d\n", c);
-		  fflush(fifo2_in);
-		} else {
-		  fprintf(fout, "%d\n", res);
-		  fflush(fout);
-		  break;
-		}
-	}
-	sc2 = (res == a + b + c) ? 1.0 : 0.0;
+    while (true) {
+        fscanf(fifo2_out, "%d", &res);
+        if (res < 0) {
+          fprintf(fifo2_in, "%d\n", c);
+          fflush(fifo2_in);
+        } else {
+          fprintf(fout, "%d\n", res);
+          fflush(fout);
+          break;
+        }
+    }
+    sc2 = (res == a + b + c) ? 1.0 : 0.0;
 
-	printf("%.2lf %.2lf\n", sc1, sc2);
+    printf("%.2lf %.2lf\n", sc1, sc2);
 
-	fclose(fin);
-	fclose(fout);
-	fclose(fifo1_in);
-	fclose(fifo1_out);
-	fclose(fifo2_in);
-	fclose(fifo2_out);
+    fclose(fin);
+    fclose(fout);
+    fclose(fifo1_in);
+    fclose(fifo1_out);
+    fclose(fifo2_in);
+    fclose(fifo2_out);
 }
 
--- pvp/sol/stub.cpp
+++ pvp/sol/stub.cpp
@@ -8,29 +8,29 @@ static FILE *fifo_in, *fifo_out;
 int add(int a, int b);
 
 int provide_random_number() {
-  int c;
-  // Sending -1 means that we are requiring a random number from the
-  // manager, not that we are submitting the solution.
-  fprintf(fifo_out, "-1\n");
-  fflush(fifo_out);
-  assert(fscanf(fifo_in, "%d", &c) == 1);
-  return c;
+    int c;
+    // Sending -1 means that we are requiring a random number from the
+    // manager, not that we are submitting the solution.
+    fprintf(fifo_out, "-1\n");
+    fflush(fifo_out);
+    assert(fscanf(fifo_in, "%d", &c) == 1);
+    return c;
 }
 
 int main(int argc, char **argv) {
 
-	fifo_in = fopen(argv[1], "r");
-	fifo_out = fopen(argv[2], "w");
+    fifo_in = fopen(argv[1], "r");
+    fifo_out = fopen(argv[2], "w");
 
-	int a, b;
-	assert(fscanf(fifo_in, "%d %d", &a, &b) == 2);
-	fprintf(fifo_out, "%d\n", add(a, b));
-	fflush(fifo_out);
+    int a, b;
+    assert(fscanf(fifo_in, "%d %d", &a, &b) == 2);
+    fprintf(fifo_out, "%d\n", add(a, b));
+    fflush(fifo_out);
 
-	fclose(fifo_in);
-	fclose(fifo_out);
+    fclose(fifo_in);
+    fclose(fifo_out);
 
-	return 0;
+    return 0;
 
 }
 
